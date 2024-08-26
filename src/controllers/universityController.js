5999603f19469293c3b5446e6dc427c70f7f2348
ArmanJumashev <124795505+ArmanJumashev@users.noreply.github.com>
2024-08-26 17:43:34 +0500

    arman 26.08

--- src/controllers/universityController.js
+++ src/controllers/universityController.js
@@ -13,12 +13,39 @@ const createUniversity = async (req, res) => {
 
 const getAllUniversities = async (req, res) => {
     try {
-        const universities = await University.find();
-        res.json(universities);
+        // Extract and normalize the query parameters
+        console.log(req.query);
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const status_tag = req.query.statusFilter || null;
+        console.log('1');
+        // Create a filter object based on status_tag if provided
+        const filter = {};
+        if (status_tag && status_tag !== 'Все') {
+            filter.status_tag = status_tag;
+        }
+
+        // Calculate the skip value for pagination
+        const skip = (page - 1) * limit;
+
+        // Fetch the universities with the given filter, limit, and skip
+        const universities = await University.find(filter)
+            .skip(skip)
+            .limit(limit);
+        console.log(universities.length);
+        // Get the total count of universities with the given filter
+        const totalUniversities = await University.countDocuments(filter);
+
+        res.json({
+            universities,
+            totalPages: Math.ceil(totalUniversities / limit),
+            currentPage: page,
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
+
 
 const getUniversityById = async (req, res) => {
     try {
